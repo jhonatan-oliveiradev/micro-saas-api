d3fcfe69ca3a834739b2b0d7796f091ff2e76738
jhonatan-oliveiradev <jhonatan.oliveira931002@gmail.com>
2024-04-22 12:14:19 -0300

    feat: Add relation todo to user and create User Controller

--- source/controllers/todo.controller.ts
+++ source/controllers/todo.controller.ts
@@ -1,9 +1,35 @@
 import type { Request, Response } from "express";
+import { prisma } from "../lib/prisma";
 
-export const createTodoController = (request: Request, response: Response) => {
+export const createTodoController = async (
+	request: Request,
+	response: Response
+) => {
 	const userId = request.headers["x-user-id"];
 
 	if (!userId) {
-		return response.status(403).json({ error: "Unauthorized" });
+		return response.status(403).send({
+			error: "Unauthorized"
+		});
 	}
+
+	const user = await prisma.user.findUnique({
+		where: {
+			id: userId as string
+		}
+	});
+
+	if (!user) {
+		return response.status(403).send({
+			error: "Unauthorized"
+		});
+	}
+
+	const { title } = request.body;
+
+	const todo = await prisma.todo.create({
+		data: {
+			title
+		}
+	});
 };
